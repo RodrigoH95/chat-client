b52df96939c286c81cd7bb51c2c44158f2187fa7
RodrigoH95 <HRodrigo95@hotmail.com>
2023-01-17 02:50:54 -0300

    update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const salas = document.getElementById("salas-list");
 const users = document.getElementById("users");
 const currentRoom = document.getElementById("room-name");
 let url = null;
-const DEBUG = false;
+const DEBUG = true;
 
 
 if (DEBUG) {
@@ -17,8 +17,10 @@ if (DEBUG) {
 const socket = io(url);
 
 let nombre = "";
+let history = [];
+let userRoom = "lobby";
 
-messageContainer.innerHTML = "Aguarda unos segundos mientras te conectas a la sala...";
+infoMessage("Aguarda unos segundos mientras te conectas a la sala...");
 
 const user = localStorage.getItem("user");
 if(user) {
@@ -28,11 +30,19 @@ if(user) {
 }
 
 socket.on("connect", () => {
-  messageContainer.innerHTML = "Te uniste al chat";
+  infoMessage("Te uniste al chat");
   socket.emit("new-user");
   crearNuevaSala("lobby", "Lobby");
-  socket.emit("join-room", "lobby", nombre);
+  loadData();
+  socket.emit("join-room", userRoom, nombre);
+  
 });
+
+function loadData() {
+  const previousMessages = JSON.parse(window.localStorage.getItem("history"));
+  if(previousMessages) history = previousMessages;
+  loadCurrentRoom();
+}
 
 socket.on("room-list", lista => {
   const lobby = [...salas.childNodes].find(node => node.id === "lobby");
@@ -49,11 +59,19 @@ socket.on("user-join-room", (socketID, roomName, playerName) => {
   } else {
     message = `${playerName} se une`;
   }
-  messageContainer.innerHTML += `<p>${message} a la sala ${roomName}</p>`;
+  if(socketID === socket.id) {
+    if(history.findIndex(room => room.id === roomName) === -1) {
+      history.push({id: roomName, messages: []})
+    } else {
+      messageContainer.innerHTML = "";
+      loadMessages(roomName);
+    }
+  }
+  infoMessage(`${message} a la sala ${roomName}`);
 });
 
 socket.on("user-leaves-room", userName => {
-  messageContainer.innerHTML += `<p>${userName} deja la sala</p>`;
+  infoMessage(`${userName} deja la sala`);
 })
 
 socket.on("room-users", (userList) => {
@@ -67,10 +85,28 @@ socket.on("room-users", (userList) => {
 socket.on("receive-message", message => {
   // Llega mensaje con id, sender, message y time
  displayMessage(message, socket);
-})
+ addToHistory(socket, message);
+});
+
+function addToHistory(socket, message) {
+  const copy = {...message};
+  if(copy.id === socket.id) {
+    copy.sender = "Yo"
+  }
+  history.filter(room => String(room.id) === currentRoom.innerText)[0].messages.push(copy);
+  window.localStorage.setItem("history", JSON.stringify(history));
+}
+
+function loadMessages(roomID) {
+  history.filter(room => room.id === roomID)[0].messages.forEach(message => displayMessage(message));
+}
 
 socket.on("room-full", () => {
-  messageContainer.innerHTML += `<p>La sala está llena</p>`;
+  infoMessage("La sala está llena");
+})
+
+socket.on("room-not-found", () => {
+  infoMessage("No se encontró la sala");
 })
 
 chatBox.addEventListener("submit", e => {
@@ -80,14 +116,33 @@ chatBox.addEventListener("submit", e => {
   input.value = "";
 });
 
+function infoMessage(message) {
+  messageContainer.innerHTML += `<p>${message}</p>`;
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
 function crearNuevaSala(id, roomName) {
   let sala = document.createElement("div");
   sala.classList.add("sala");
   sala.id = id;
-  console.log(roomName);
   sala.innerText = roomName;
-  sala.onclick = () => socket.emit("join-room", id, nombre);
+  sala.onclick = () => {
+    setCurrentRoom(id);
+    socket.emit("join-room", id, nombre)
+  };
   salas.appendChild(sala);
+}
+
+function loadCurrentRoom() {
+  const room = localStorage.getItem("currentRoom");
+  if(room) {
+    userRoom = room;
+  }
+}
+
+function setCurrentRoom(id) {
+  userRoom = id;
+  localStorage.setItem("currentRoom", userRoom);
 }
 
 function crearNuevoUsuario(nombre, isCurrentUser = false) {
@@ -112,14 +167,18 @@ function cambiarNombre() {
   socket.emit("user-change-name", room, nombre);
 }
 
-function displayMessage(message, socket) {
+function displayMessage(message, socket = null) {
   const m = document.createElement("div");
-  m.classList.add("message");
-  socket.id === message.id ? m.classList.add("sent") : m.classList.add("received");
 
-  const sender = document.createElement("div");
-  sender.classList.add("msg-sender");
-  sender.innerText = socket.id === message.id ? "Yo:" : message.sender + ":";
+  const senderInfo = document.createElement("div");
+  let sender;
+  if(socket && socket.id === message.id) {
+    sender = "Yo";
+  } else {
+    sender = message.sender;
+  }
+  senderInfo.classList.add("msg-sender");
+  senderInfo.innerText = sender + ":";
 
   const content = document.createElement("div");
   content.classList.add("msg-content");
@@ -129,7 +188,10 @@ function displayMessage(message, socket) {
   time.classList.add("msg-time");
   time.innerText = message.time;
 
-  m.appendChild(sender);
+  m.classList.add("message");
+  sender === "Yo" ? m.classList.add("sent") : m.classList.add("received");
+
+  m.appendChild(senderInfo);
   m.appendChild(content);
   m.appendChild(time);
 
